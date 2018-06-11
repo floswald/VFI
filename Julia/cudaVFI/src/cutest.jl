3d88b16f4a8b096dc394303da7d86ade0e46cf21
florian oswald <florian.oswald@gmail.com>
2018-06-11 08:46:25 +0200

    cutest

--- Julia/cudaVFI/src/cutest.jl
+++ Julia/cudaVFI/src/cutest.jl
@@ -1,4 +1,6 @@
 
+# /opt/cuda-8.0/samples/1_Utilities/deviceQuery/deviceQuery
+
 
 function launcher(f::Function,nthread)
 
@@ -16,6 +18,29 @@ end
 
 
 function blockid(y::CuDeviceVector{Int})
+	idx = (blockIdx().x-1) * blockDim().x + threadIdx().x
+	y[idx] = blockIdx().x
+end
+
+function launcher2d(f::Function,nthread)
+
+	x = CuArray(zeros(Int,8,2))
+	y = CuArray(zeros(Int,8,2))
+	blocks = 4
+
+    @cuda blocks=blocks threads=nthread f(x,y)
+    return x 
+end
+
+function kernelid2d(x::CuDeviceMatrix{Int},y::CuDeviceMatrix{Int})
+	ix = (blockIdx().x-1) * blockDim().x + threadIdx().x
+	iy = (blockIdx().y-1) * blockDim().y + threadIdx().y
+	x[ix,iy] = threadIdx().x
+	y[ix,iy] = threadIdx().y
+end
+
+
+function blockid2d(y::CuDeviceVector{Int})
 	idx = (blockIdx().x-1) * blockDim().x + threadIdx().x
 	y[idx] = blockIdx().x
 end
